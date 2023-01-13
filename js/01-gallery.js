436e285f8d6f0ebee49f082735bbd5443c85a99e
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-01-13 18:46:04 +0200

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML(
+  "afterbegin",
+  createGalleryMarkupFromArray(galleryItems)
+);
+
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function createGalleryMarkupFromArray(itemArray) {
+  return itemArray
+    .map((item) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const modal = window.basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+`, {
+    onShow: () => {window.addEventListener('keydown', onKeyEscapeClick)},
+    onClose: () => {window.removeEventListener('keydown', onKeyEscapeClick)},
+});
+
+  modal.show();
+
+  function onKeyEscapeClick(event) {
+    // console.log(event.code);
+    if (event.code === "Escape") {
+      modal.close();
+    }
+    return;
+  };
+};
